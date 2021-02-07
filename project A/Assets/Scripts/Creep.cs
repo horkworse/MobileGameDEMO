8e4b6af134abc45bdfbf0502643eb23397de42de
horkworse <horkworse@gmail.com>
2021-02-07 16:52:18 +0500

    add скууз interaction area

--- project A/Assets/Scripts/Creep.cs
+++ project A/Assets/Scripts/Creep.cs
@@ -9,22 +9,64 @@ public class Creep : Character
     public NavMeshAgent agent;
     public static bool speed_change = false;
 
+    public float sightRange;
+    public bool playerInSightRange;
+
+    public LayerMask whatIsPlayer;
+    public LayerMask whatIsGround;
+
+    public bool walkPointSet;
+    public float walkPointRange;
+
+    Vector3 walkPoint;
+
     void Start () {
         agent = GetComponent<NavMeshAgent>();
     }
 
     void Update(){
-        // Смерть крипа
+
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+
+        if (playerInSightRange && HP > 0) MoveToPlayer();
+        else if (!playerInSightRange) Patrolling();
         if (HP <= 0) Destroy(gameObject);
-        // Следование на позицию цели
-        if (HP > 0)
-        agent.destination = goal.position;
         if (speed_change == true)
         {
             agent.speed = 0;
             speed_change = false;
         }
-        
+    }
+
+    void Patrolling()
+    {
+        // не удалять, доделает hork
+
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (!walkPointSet) agent.SetDestination(walkPoint);
+
+        var distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    void SearchWalkPoint()
+    {
+        // не удалять, доделает hork
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    void MoveToPlayer()
+    {
+        agent.destination = goal.position;
     }
     private void OnTriggerEnter(Collider other)
     {
